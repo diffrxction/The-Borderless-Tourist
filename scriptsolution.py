c70ed4b559add353a8721bbdfab4fc687c03cecd
Aryan Pareek <65293175+diffrxction@users.noreply.github.com>
2020-09-06 23:50:22 +0530

    Update scriptsolution.py

--- scriptsolution.py
+++ scriptsolution.py
@@ -1,3 +1,5 @@
+# This is the complete solution for The-Borderless-Tourist. :)
+
 destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
 test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
 def get_destination_index(destination):
